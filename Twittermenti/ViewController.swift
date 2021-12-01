9b79d11414e38a0370fe842db940faf423f1649b
Yasser Hajlaoui <yasser159@hotmail.com>
2021-11-30 16:24:49 -0800

    added API Key and Plist key getter functionality

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -7,15 +7,66 @@
 //
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    
+    //-----------------------
+    private var apiKey: String {
+        get {
+            guard let filePath = Bundle.main.path(forResource: "Secret", ofType: "plist") else {
+                    fatalError("Couldn't find file 'Config.plist'.")
+        }
+     
+            let plist = NSDictionary(contentsOfFile: filePath)
+            guard let value = plist?.object(forKey: "API-Key") as? String else {
+                    fatalError("Couldn't find key 'API-Key' in 'Secret.plist'.")
+            }
+            return value
+        }
+    }
+     
+    private var apiKeySecret: String {
+        get {
+            guard let filePath = Bundle.main.path(forResource: "Secret", ofType: "plist") else {
+                    fatalError("Couldn't find file 'Config.plist'.")
+        }
+     
+            let plist = NSDictionary(contentsOfFile: filePath)
+            guard let value = plist?.object(forKey: "API-Secret") as? String else {
+                    fatalError("Couldn't find key 'API-Secret' in 'Secret.plist'.")
+            }
+            return value
+        }
+    }
+     
+    var swifter = Swifter(consumerKey: "dummy key", consumerSecret: "dummy Secret")
+    
+    //------------------------
 
+    //let swifter = Swifter(consumerKey: "QQUM1jrh1qgDnjqg51eMogXMl", consumerSecret: "cPXoQsCyxfwmyjS1y59lmpWTLscfnkLMzeNO0wRBmzHrIf2Xl7")
+    
     override func viewDidLoad() {
+         
+        swifter = Swifter(consumerKey: apiKey, consumerSecret: apiKeySecret)
+        
+        // Do any additional setup after loading the view.
         super.viewDidLoad()
+        
+
+        
+        swifter.searchTweet(using: "@Apple", success: { (results, metadata) in
+            
+            print(results)
+        
+        }) { (error) in
+            print("There was an error with the Twitter API Request, \(error)")
+        }
         
     }
 
